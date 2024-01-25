de30d1b6e9fe266f05c11a8b975a34ec1c0f2372
Mohammed-Mujaheed <151029580+Mohammed-Mujaheed@users.noreply.github.com>
2024-01-25 15:25:19 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 14458
+let accountEmail = "mj@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 4
+// const accountId2 = 78987
+accountEmail = "mc@ms.com"
+accountPassword = "12121212"
+accountCity = "Bengaluru"
+let accountState
+
+// console.log(accountId);
+// console.log(accountEmail);
+ console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
