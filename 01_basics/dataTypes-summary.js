4bf192924360fcb6069e755854127e70009a605b
Mohammed Mujaheed <151029580+Mohammed-Mujaheed@users.noreply.github.com>
2024-01-27 12:24:19 +0000

    dataTypes-summary done

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,23 @@
+// primitive
+
+// 7 types : String,Number,
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+// console.log(typeof anotherId);
+console.log(typeof heros);
+console.log(typeof myObj);
+// console.log(myFunction);
