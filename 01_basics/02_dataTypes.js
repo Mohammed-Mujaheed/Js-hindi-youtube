0e095fc4fc462198aeb22bcf5ae036349178eae1
Mohammed Mujaheed <151029580+Mohammed-Mujaheed@users.noreply.github.com>
2024-01-27 08:52:27 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let Name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+console.table([Name,age,isLoggedIn,state])
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.log(typeof "hitesh"); // object
