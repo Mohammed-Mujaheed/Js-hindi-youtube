59b250f6add44d857e965cdec10503166bfea5e8
Mohammed Mujaheed <151029580+Mohammed-Mujaheed@users.noreply.github.com>
2024-01-27 11:18:54 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,25 @@
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1); //here js will automatically 
+                    // -converts string into number
+
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+
+// === 
+
+console.log("2" === 2);  // here js will strictly check type of data if 
+                        // if unmatch wil return false
