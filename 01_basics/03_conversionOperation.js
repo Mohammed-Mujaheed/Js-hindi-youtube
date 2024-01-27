4bedf12ea4c6a568557f14b3c738ffcd3b13cb99
Mohammed Mujaheed <151029580+Mohammed-Mujaheed@users.noreply.github.com>
2024-01-27 10:57:06 +0000

    Conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,66 @@
+let score = 99
+ 
+// console.log(typeof score)
+// console.log(typeof(score))
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+/*
+
+"33" => 33
+"33abc" => NaN
+true => 1; false => 0
+
+*/
+
+let isLoggedIn = " "
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof(booleanIsLoggedIn))
+// console.log(booleanIsLoggedIn)
+
+/*
+
+1 => true; 0 => false
+"" => false
+"Mujaheed" => true
+
+ */
+
+let someNumber = 34
+let stringNumber = String(someNumber)
+// console.log(typeof stringNumber)
+// console.log(stringNumber)
+
+
+
+// ********************* Operations *****************************
+
+
+let value = 8
+let negValue = -value
+// console.log("The Negative Value of 8 is => "+negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*3)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(3%2)
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(+true);
+// console.log(true+);  will show erorr in output
+// console.log(+"");
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
