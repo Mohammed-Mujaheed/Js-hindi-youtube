9d0eb6ee37a9c2eaaa477e8327aac7da7f6a7bbf
Mohammed Mujaheed <151029580+Mohammed-Mujaheed@users.noreply.github.com>
2024-02-07 10:49:09 +0000

    Math and num

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,3 +1,44 @@
 
 // console.log('Mujaheed')
 // console.log(2+2)
+
+const score = 400
+// console.log(score);
+// console.log(typeof score)
+
+let balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toString())
+// console.log(typeof balance.toString())
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+// console.log(hundreds.toLocaleString());
+
+//MATH
+
+// console.log(Math)
+// console.log(Math.abs(-3))
+// console.log(Math.ceil(4.3))
+// console.log(Math.floor(4.9))
+// console.log(Math.min(3, 5, 2, 6,))
+// console.log(Math.max(3, 5, 2, 6,))
+
+console.log(Math.random())
+// console.log(Math.random()*10)
+// console.log((Math.random()*10) + 1)
+// console.log(Math.floor(Math.random()*10) + 1);
+
+let min = 10
+let max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min )
+console.log(Math.floor(Math.random() * (max - min + 1 )) + min )
+// console.log((Math.random() * (20 - 10 + 1)) + min )
