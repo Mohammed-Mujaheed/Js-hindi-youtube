ce24ef9aa16c6ab1268e5a8a8ee47edf2e261a2a
Mohammed Mujaheed <151029580+Mohammed-Mujaheed@users.noreply.github.com>
2024-02-11 07:45:20 +0000

    Arrays in js

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,5 +1,37 @@
 // node 02_basics/01_arrays.js
 
-const myArr = [0, 1, 2, 3, 4, 5]
-console.log(myArr)
-console.log(myArr[2])
+// const myArr = [0, 1, 2, 3, 4, 5]
+// console.log(myArr)
+// console.log(myArr[2])
+// console.log(myArr[6])
+
+// const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr2[2])
+// console.log(myArr2[5])
+
+// Arrays methods
+
+// myArr.push(11)
+// myArr.push(8)
+// myArr.pop()
+// myArr.push(11)
+// console.log(myArr)
+
+// myArr.unshift(9)
+// myArr.shift()
+// console.log(myArr)
+
+//slice and splice
+
+// console.log("A", myArr);
+// const myn1 = myArr.slice(1, 3);
+// console.log(myn1);
+// console.log("B", myArr);
+// // console.log("\n")
+
+// // console.log("C", myArr);
+// const myn2 = myArr.splice(1, 3);
+// console.log("C", myArr);
+// console.log(myn2);
+
+
---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,40 @@
+
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+// console.log(dc_heros);
+// console.log(marvel_heros[3][0]);
+// console.log(typeof marvel_heros);
+// console.log(typeof (marvel_heros));
+
+// ways to merge two arrays into one
+//1
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros)
+
+//2
+// const all_new_heros = [...marvel_heros, ...dc_heros]
+// const all_new_heros = [...dc_heros, ...marvel_heros]
+// console.log(all_new_heros)
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+// const real_another_array = another_array.flat(Infinity);
+const real_another_array = another_array.flat(5);
+
+// const real_another_array = another_array.flat(3);
+// console.log(real_another_array);
+
+console.log(Array.isArray("Mujaheed"))
+
+// console.log(Array.from({name: "Mujaheed"})) //Interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
+
+
+
