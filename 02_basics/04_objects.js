0833ca0956a37893e6fe89261f40bf9760ac34cd
Mohammed Mujaheed <151029580+Mohammed-Mujaheed@users.noreply.github.com>
2024-02-15 12:35:52 +0000

    obj

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -29,10 +29,32 @@ const regularUser = {
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
-// const obj4 = {5: "a", 6: "b"}
+const obj4 = {5: "a", 6: "b"}
 
 // const obj3 = {obj1, obj2}
-const obj3 = Object.assign({obj1, obj2)
+// const obj3 = Object.assign(obj2, obj1, obj4)
+
 // const obj3 = Object.assign({}, obj1, obj2, obj4)
 
-console.log(obj3)
+const obj3 = {...obj1, ...obj2, ...obj4}
+// console.log(obj3)
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+console.log(tinderUser)
+console.log(users[1])
+console.log(users[1].email)
