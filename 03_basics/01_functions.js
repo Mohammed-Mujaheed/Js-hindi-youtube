4f8cd5d7e453c673ab5cdcc4655302822aad624f
Mohammed Mujaheed <151029580+Mohammed-Mujaheed@users.noreply.github.com>
2024-02-16 19:39:48 +0000

    functions in js

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,23 @@
+function sayMYName(){
+
+    console.log("M");
+    console.log("U");
+    console.log("J");
+    console.log("A");
+    console.log("H");
+    console.log("E");
+    console.log("E");
+    console.log("D");
+
+}
+
+// sayMYName()
+// sayMYName --> it is just a reference 
+// sayMYName() --> it's both reference i.e sayMyName + execute i.e ()
+
+function addTwoNumbers(n1 , n2){
+    // console.log(3+5);
+    console.log(n1 + n2);
+}
+
+addTwoNumbers(a, 9)
