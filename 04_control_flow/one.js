c8d04e22c8958e9bfab20d0793a85a2fca983198
Mohammed Mujaheed <151029580+Mohammed-Mujaheed@users.noreply.github.com>
2024-02-26 11:44:25 +0000

    control_flow part2

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -18,11 +18,38 @@
 
 // console.log("finally executed")
 
-const score = 200
-if (score > 100) {
-    var power = "fly"
-    console.log(`User power: ${power}`);    
+// const score = 200
+// if (score > 100) {
+//     const power = "fly" // it will give error for line 27th
+//    or // var power = "fly"  // it will execute the code along with 27th line
+//     console.log(`User power: ${power}`);    
+// }
+// console.log(`User power: ${power}`);
+
+// const balance = 1000
+// if(balance > 500) console.log("test");
+// if(balance > 500) console.log("test"),console.log("test2");
+
+// if(balance < 500){
+//     console.log("less than 500");
+// } else if(balance < 750){
+//     console.log("less than 750")
+// } else if (balance < 900){
+//     console.log("less than 900")
+// } else {
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==2) {
+    console.log("Allow to buy courses");
 }
 
-console.log(`User power: ${power}`);
-
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+    
+}
