492185263d5667c6c0a47502417995733ff1de9b
Mohammed Mujaheed <151029580+Mohammed-Mujaheed@users.noreply.github.com>
2024-02-26 10:38:51 +0000

    controlfow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,28 @@
+// node 04_control_flow/one.js
+
+// comparasions
+// <, >, <=, >=, ==, !=, ===, !==
+// if
+
+
+// if(2 !== "2"){
+//     // console.log("janabe aali Mohammed")
+// }
+
+// const temp = 70;
+// if(temp < 60){
+//     console.log("temp is less than 60")
+// } else {
+//     console.log("temp is greater than 60")
+// }
+
+// console.log("finally executed")
+
+const score = 200
+if (score > 100) {
+    var power = "fly"
+    console.log(`User power: ${power}`);    
+}
+
+console.log(`User power: ${power}`);
+
