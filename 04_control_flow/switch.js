20d0a8a5d2da0e540541fb8b675fdb6fe2b81c38
Mohammed Mujaheed <151029580+Mohammed-Mujaheed@users.noreply.github.com>
2024-02-26 12:51:14 +0000

    truthy and falsy values

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -9,22 +9,52 @@
 
 const month = "march"
 
-switch (month) {
-    case "jan":
-        console.log("January");
+// switch (month) {
+//     case "jan":
+//         console.log("January");
+//         break;
+//     case "feb":
+//         console.log("feb");
+//         break;
+//     case "march":
+//         console.log("march");
+//         break;
+//     case "april":
+//         console.log("april");
+//         break;
+
+//     default:
+//         console.log("default case match");
+//         break;
+// }
+
+const day = 20;
+
+switch (day) {
+    case 1:
+        console.log("Monday")
         break;
-    case "feb":
-        console.log("feb");
+    case 2:
+        console.log("Tuesday")
         break;
-    case "march":
-        console.log("march");
+    case 3:
+        console.log("Wednesday")
         break;
-    case "april":
-        console.log("april");
+    case 4:
+        console.log("Thursday")
+        break;
+    case 5:
+        console.log("Friday")
+        break;
+    case 6:
+        console.log("Saturday")
+        break;
+    case 7:
+        console.log("Sunday")
         break;
 
     default:
-        console.log("default case match");
+        console.log("Invalid day")
         break;
 
 }
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,52 @@
+//it's nothing but implicitly assuming that a value is true or fasle 
+ 
+
+//falsy values
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// truthy values
+// "0", 'fasle', " ", [], {}, function(){}
+
+// examples
+
+// const userEmail = "mj@gmail.com"
+// if (userEmail) {
+//     console.log("Got user email");
+// } else {
+//     console.log("Don't have user email")
+// }
+
+// const num1 = "0"
+// if (num1) {
+//     console.log(`${num1} is a truthy value`)   
+// } else {
+//     console.log(`${num1} is a falsy value`)
+// }
+
+const emptyObj = {}
+
+if(Object.keys(emptyObj).length === 0){
+    // console.log("Object is empty");
+
+}
+
+ //or
+// if(Object.values(emptyObj).length === 0){
+//     console.log("Object is empty");
+// }
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+//  val1 = 5 ?? 10
+//  val1 = null ?? 10
+//  val1 = undefined ?? 15
+// val1 = null ?? 10 ?? 20
+
+// console.log(val1)
+
+// Terniary Operator 
+//condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
